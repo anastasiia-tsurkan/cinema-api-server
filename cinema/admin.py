51d61c4bb6141eb25ba5b2dbacd4bc5c962d6624
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-04-26 23:21:40 +0300

    Register cinema models in admin.py

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    CinemaHall,
+    Genre,
+    Actor,
+    Movie,
+    MovieSession,
+    Order,
+    Ticket,
+)
+
+admin.site.register(CinemaHall)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Movie)
+admin.site.register(MovieSession)
+admin.site.register(Order)
+admin.site.register(Ticket)
